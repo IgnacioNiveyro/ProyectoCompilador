2be8a2c7880edf06c4f068966c441e8c6c3b787b
IgnacioNiveyro <nacho_niveyro@hotmail.com>
2023-08-20 14:37:20 -0300

    Implementando el manejador de archivos

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
+import ManejadorDeArchivo.ManejadorDeArchivo;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        
+        File file;
+        ManejadorDeArchivo manejadorDeArchivo = null;
+
+        file = new File("src/prueba.txt");
+        try {
+            manejadorDeArchivo = new ManejadorDeArchivo(file);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        char caracterLeido = (char)manejadorDeArchivo.obtenerCaracterACtual();
+        System.out.println(!manejadorDeArchivo.endOfFile());
+        while(manejadorDeArchivo.endOfFile()){
+
+            System.out.println(caracterLeido);
+            try {
+                caracterLeido = (char) manejadorDeArchivo.leerProximoCaracter();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
---
+++ src/ManejadorDeArchivo/ManejadorDeArchivo.java
@@ -0,0 +1,41 @@
+package ManejadorDeArchivo;
+
+import java.io.*;
+
+public class ManejadorDeArchivo {
+    private File file;
+    private int numeroLineaActual = 1;
+    private int caracterActual;
+    BufferedReader reader;
+
+    public ManejadorDeArchivo(File file) throws IOException {
+        this.file = file;
+        FileReader fileReader = new FileReader(file);
+        reader = new BufferedReader(fileReader);
+        this.caracterActual = reader.read();
+    }
+
+    public int leerProximoCaracter() throws IOException {
+        if(this.caracterActual == '\r' || this.caracterActual == '\n')
+            this.numeroLineaActual += 1;
+
+        this.caracterActual = this.reader.read();
+
+        if(this.caracterActual == '\r')
+            this.caracterActual = this.reader.read();
+
+        return this.caracterActual;
+    }
+
+    public int obtenerNumeroLineaActual(){
+        return this.numeroLineaActual;
+    }
+
+    public int obtenerCaracterACtual(){
+        return this.caracterActual;
+    }
+
+    public boolean endOfFile(){
+        return this.caracterActual != -1;
+    }
+}
